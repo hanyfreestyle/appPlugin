6a38758bfb40d4aea06dfc940a58d4a469207124
Hany Darwish <hany.freestyle4u@gmail.com>
2024-02-23 21:53:18 +0200

    Sours

--- ConfigMeta/database/migrations/2019_12_14_000008_create_web_privacies_table.php
+++ ConfigMeta/database/migrations/2019_12_14_000008_create_web_privacies_table.php
@@ -6,20 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+
     public function up(): void
     {
-        Schema::create('config_meta_tags', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('cat_id')->unique()->index();
-            $table->string('photo')->nullable();
-            $table->string('photo_thum_1')->nullable();
+        Schema::create('config_web_privacies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string("name");
+            $table->integer("postion")->default(0);
+            $table->boolean("is_active")->default(true);
             $table->timestamps();
             $table->softDeletes();
         });
+
     }
+
 
     public function down(): void
     {
-        Schema::dropIfExists('config_meta_tags');
+        Schema::dropIfExists('config_web_privacies');
     }
 };
