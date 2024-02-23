99ef94ad7521e2646dac7dd2bfa7544f6dd56c7a
Hany Darwish <hany.freestyle4u@gmail.com>
2024-02-23 17:40:46 +0200

    Sours

---
+++ DataNewsLetter/app/AppPlugin/Leads/NewsLetter/NewsLetter.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\AppPlugin\Leads\NewsLetter;
+
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+
+class NewsLetter extends Model{
+
+    protected $table = "leads_news_letters";
+    protected $fillable = ['email'];
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#|||||||||||||||||||||||||||||||||||||| #
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    public function getFormatteDate(){
+        return Carbon::parse($this->created_at )->locale(app()->getLocale())->translatedFormat('jS M Y') ;
+    }
+}
---
+++ DataNewsLetter/resources/lang/en/admin/newsletter.php
@@ -0,0 +1,8 @@
+<?php
+
+return
+[
+	'menu' => 'NewsLetter',
+	't_email' => 'Email Address',
+	't_date_add' => 'Register Data',
+];
