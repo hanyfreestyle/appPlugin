26b408b6016da5ad68a93c6a583eb1fbcae78390
Hany Darwish <hany.freestyle4u@gmail.com>
2024-02-26 19:40:44 +0200

    Sours

--- LeadsNewsLetter/app/AppPlugin/Leads/NewsLetter/NewsLetterController.php
+++ LeadsNewsLetter/app/AppPlugin/Leads/NewsLetter/NewsLetterController.php
@@ -13,65 +13,65 @@ use Maatwebsite\Excel\Facades\Excel;
 
 class NewsLetterController extends AdminMainController {
 
-  use CrudTraits;
+    use CrudTraits;
 
-  function __construct(NewsLetter $model) {
+    function __construct(NewsLetter $model) {
 
-    parent::__construct();
-    $this->controllerName = "NewsLetter";
-    $this->PrefixRole = 'config';
-    $this->selMenu = "config.";
-    $this->PrefixCatRoute = "";
-    $this->PageTitle = __('admin/newsletter.menu');
-    $this->PrefixRoute = $this->selMenu . $this->controllerName;
-    $this->model = $model;
+        parent::__construct();
+        $this->controllerName = "NewsLetter";
+        $this->PrefixRole = 'config';
+        $this->selMenu = "config.";
+        $this->PrefixCatRoute = "";
+        $this->PageTitle = __('admin/leadsNewsLetter.app_menu');
+        $this->PrefixRoute = $this->selMenu . $this->controllerName;
+        $this->model = $model;
 
-    $sendArr = [
-      'TitlePage' => $this->PageTitle,
-      'PrefixRoute' => $this->PrefixRoute,
-      'PrefixRole' => $this->PrefixRole,
-      'AddConfig' => true,
-      'configArr' => ["filterid" => 0, "datatable" => 0, 'orderbyName' => 0, "orderbyDate" => 1],
-      'restore' => 0,
-      'AddAction' => 0,
-      'formName' => "NewsLetter",
-    ];
-    self::loadConstructData($sendArr);
-    $this->middleware('permission:' . $this->PrefixRole . '_defPhoto_view', ['only' => ['index']]);
-  }
+        $sendArr = [
+            'TitlePage' => $this->PageTitle,
+            'PrefixRoute' => $this->PrefixRoute,
+            'PrefixRole' => $this->PrefixRole,
+            'AddConfig' => true,
+            'configArr' => ["filterid" => 0, "datatable" => 0, 'orderbyName' => 0, "orderbyDate" => 1],
+            'restore' => 0,
+            'AddAction' => 0,
+            'formName' => "NewsLetter",
+        ];
+        self::loadConstructData($sendArr);
+        $this->middleware('permission:' . $this->PrefixRole . '_defPhoto_view', ['only' => ['index']]);
+    }
 
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #|||||||||||||||||||||||||||||||||||||| # index
-  public function index(Request $request) {
-    $pageData = $this->pageData;
-    $pageData['ViewType'] = "List";
+    public function index(Request $request) {
+        $pageData = $this->pageData;
+        $pageData['ViewType'] = "List";
 
-    $session = self::getSessionData($request);
+        $session = self::getSessionData($request);
 
-    if($session == null) {
-      $rowData = self::getSelectQuery(NewsLetter::query());
-    } else {
-      $rowData = self::getSelectQuery(self::FilterQ(NewsLetter::query(), $session));
+        if($session == null) {
+            $rowData = self::getSelectQuery(NewsLetter::query());
+        } else {
+            $rowData = self::getSelectQuery(self::FilterQ(NewsLetter::query(), $session));
+        }
+
+        return view('AppPlugin.LeadsNewsLetter.index', compact('pageData', 'rowData'));
     }
-
-    return view('AppPlugin.LeadsNewsLetter.index', compact('pageData', 'rowData'));
-  }
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #|||||||||||||||||||||||||||||||||||||| # Export
-  public function Export(Request $request) {
-    $pageData = $this->pageData;
-    $pageData['ViewType'] = "List";
-    $session = Session::get($this->formName);
+    public function Export(Request $request) {
+        $pageData = $this->pageData;
+        $pageData['ViewType'] = "List";
+        $session = Session::get($this->formName);
 
-    $dwonLadeFile = Excel::download(new NewsLetterExport($request), 'newslette.xlsx');
+        $dwonLadeFile = Excel::download(new NewsLetterExport($request), 'newslette.xlsx');
 
-    $UpdateState = self::FilterQ(NewsLetter::query(), $session)->where('export', 0)->get();
-    foreach ($UpdateState as $update) {
-      $update->export = 1;
-      $update->save();
+        $UpdateState = self::FilterQ(NewsLetter::query(), $session)->where('export', 0)->get();
+        foreach ($UpdateState as $update) {
+            $update->export = 1;
+            $update->save();
+        }
+        return $dwonLadeFile;
     }
-    return $dwonLadeFile;
-  }
 
 
 }
--- LeadsNewsLetter/resources/lang/en/admin/newsletter.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return
-[
-	'menu' => 'NewsLetter',
-	't_email' => 'Email Address',
-	't_date_add' => 'Register Data',
-];
